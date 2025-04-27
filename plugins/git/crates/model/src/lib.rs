3ab6d5e5c4d200ce790540ae5d7495624b6f5d7b
Andreas Schaeffer <andreas@schaeffernet.de>
2025-04-27 18:05:59 +0200

    Moved plugins http and git from reactive-graph/std to reactive-graph/net + Refactored plugin deployment + Checkin Cargo.lock

---
+++ plugins/git/crates/model/src/lib.rs
@@ -0,0 +1,8 @@
+pub use component::repository::*;
+pub use component::transfer_progress::*;
+pub use entity::repository::*;
+
+pub mod component;
+pub mod entity;
+
+pub const NAMESPACE_GIT: &str = "git";
---
+++ plugins/http/crates/plugin/post_build.rs
@@ -0,0 +1,5 @@
+use reactive_graph_utils_deployment::deploy_plugin;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(deploy_plugin("libreactive_graph_net_http.*")?)
+}
