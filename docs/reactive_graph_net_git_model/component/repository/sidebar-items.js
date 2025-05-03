e7fba5b6794c88e9ef5e35520b89cd489668ae18
aschaeffer <aschaeffer@users.noreply.github.com>
2025-05-03 16:59:09 +0000

    deploy: 0790e57f621381679c29022ad6528f2a213f7ffc

--- docs/reactive_graph_net_git_model/component/repository/sidebar-items.js
+++ docs/reactive_graph_net_git_model/component/repository/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["BEHAVIOUR_NAME_REPOSITORY","COMPONENT_NAME_REPOSITORY"],"enum":["RepositoryProperties"],"static":["COMPONENT_REPOSITORY"],"struct":["BEHAVIOUR_REPOSITORY","COMPONENT_BEHAVIOUR_REPOSITORY"],"trait":["ComponentRepository","GitRepository"]};
+window.SIDEBAR_ITEMS = {"constant":["BEHAVIOUR_NAME_REPOSITORY","COMPONENT_NAME_REPOSITORY"],"enum":["RepositoryProperties"],"static":["BEHAVIOUR_REPOSITORY","COMPONENT_BEHAVIOUR_REPOSITORY","COMPONENT_REPOSITORY"],"trait":["ComponentRepository","GitRepository"]};
